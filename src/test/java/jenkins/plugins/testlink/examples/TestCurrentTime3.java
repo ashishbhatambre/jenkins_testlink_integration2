cfe56263f76f09ba5b16e9da8a6a10d09298d0a9
ashishbhatambre <iitm1710@gmail..com>
2014-06-19 12:29:08 +0530

    10th

--- src/test/java/jenkins/plugins/testlink/examples/TestCurrentTime3.java
+++ src/test/java/jenkins/plugins/testlink/examples/TestCurrentTime3.java
@@ -1,47 +1,131 @@
-/*
- * The MIT License
- *
- * Copyright (c) <2012> <Bruno P. Kinoshita>
- * 
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-package com.example.tests;
-
+package jenkins.plugins.testlink.examples;
+//import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+//import org.junit.*;
+import static org.junit.Assert.*;
 import org.testng.Assert;
-import org.testng.annotations.Test;
+//import static org.hamcrest.CoreMatchers.*;
+//import org.openqa.selenium.*;
+//import org.openqa.selenium.firefox.FirefoxDriver;
+//import org.openqa.selenium.support.ui.Select;
+//import org.testng.Assert;
+//import org.testng.annotations.Test;
 
-/**
- * Tests System.currentTimeMillis()
- * 
- * @author Bruno P. Kinoshita - http://www.kinoshita.eti.br
- * @since 0.3
- */
-public class TestCurrentTime3
-{
+//import java.util.Date;
+//import com.thoughtworks.selenium.*;
+//import org.testng.Reporter;
+//import org.testng.annotations.*;
+import testlink.api.java.client.TestLinkAPIResults;
 
-	@Test
-	public void testCurrentTime3() 
-	{
-		Assert.assertNotNull( System.currentTimeMillis() );
-		
-		Assert.assertTrue( System.currentTimeMillis() > 0 );
-		
+
+
+
+public class TestCurrentTime3 {
+
+	private String resultado = null;
+	  private String nota = null;
+	//private WebDriver driver;
+	  private String baseUrl;
+	  private boolean acceptNextAlert = true;
+	  private StringBuffer verificationErrors = new StringBuffer();
+
+	 // @Before
+	 /* public void setUp() throws Exception {
+	    driver = new FirefoxDriver();
+	    baseUrl = "http://cu156.cloud.maa.collab.net/";
+	    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	  }
+
+	  //@Test
+	  public void testLogin() throws Exception {
+		 
+	    driver.get(baseUrl + "/sf/sfmain/do/home");
+	    driver.findElement(By.id("username")).clear();
+	    driver.findElement(By.id("username")).sendKeys("admin");
+	    driver.findElement(By.id("pwd")).clear();
+	    driver.findElement(By.id("pwd")).sendKeys("admin");
+	    driver.findElement(By.cssSelector("strong")).click();
+	 // Warning: verifyTextPresent may require manual changes
+	    try {
+	      assertTrue(driver.findElement(By.cssSelector("BODY")).getText().matches("^[\\s\\S]*My Workspace[\\s\\S]*$"));
+	      System.out.println("passed");
+	      resultado = TestLinkAPIResults.TEST_PASSED;
+	    } catch (Error e) {
+	    	System.out.println("failed");
+	    	resultado = TestLinkAPIResults.TEST_FAILED;
+	    	nota = e.getMessage();
+	      verificationErrors.append(e.toString());
+	    }
+	   finally {
+	    	ResultadoExecucao.reportTestCaseResult1(PROJETO, PLANO, CASO_TESTE1, BUILD, nota, resultado);
+	    	}
+	  }
+	  
+
+	  //@After
+	  public void tearDown() throws Exception {
+	    driver.quit();
+	    String verificationErrorString = verificationErrors.toString();
+	    if (!"".equals(verificationErrorString)) {
+	      fail(verificationErrorString);
+	    }
+	  }
+
+	  private boolean isElementPresent(By by) {
+	    try {
+	      driver.findElement(by);
+	      return true;
+	    } catch (NoSuchElementException e) {
+	      return false;
+	    }
+	  }
+
+	  private boolean isAlertPresent() {
+	    try {
+	      driver.switchTo().alert();
+	      return true;
+	    } catch (NoAlertPresentException e) {
+	      return false;
+	    }
+	  }
+
+	  private String closeAlertAndGetItsText() {
+	    try {
+	      Alert alert = driver.switchTo().alert();
+	      String alertText = alert.getText();
+	      if (acceptNextAlert) {
+	        alert.accept();
+	      } else {
+	        alert.dismiss();
+	      }
+	      return alertText;
+	    } finally {
+	      acceptNextAlert = true;
+	    }
+	  }*/
+	 public void TestCurrentTime3()  {
+		// TODO Auto-generated method stub
+		 System.out.println("hello world");
+           //login lo=new login();
+           /*try{
+           //ResultadoExecucao re= new ResultadoExecucao();
+           //re.reportTestCaseResult1(PROJETO, PLANO, CASO_TESTE1, BUILD, nota, resultado);
+           
+           lo.setUp();
+           lo.testLogin();
+           lo.tearDown();
+		 }
+		 catch(Exception e){
+			 e.printStackTrace();
+		 }*/
+           Assert.assertNotNull( System.currentTimeMillis() );
+   		
+   		Assert.assertTrue( System.currentTimeMillis() > 0 );
+   		//System.out.println("hello world");
+		 }
+	 
+	  
 	}
-	
-}
+
+
+

c09a8813e2bfa1bb8f77f79107bc9347d044e4c5
ashishbhatambre <iitm1710@gmail..com>
2014-06-19 17:29:48 +0530

    10th

--- src/test/java/jenkins/plugins/testlink/examples/TestCurrentTime3.java
+++ src/test/java/jenkins/plugins/testlink/examples/TestCurrentTime3.java
@@ -58,11 +58,7 @@ public class TestCurrentTime3
 	  private boolean acceptNextAlert = true;
 	  private StringBuffer verificationErrors = new StringBuffer();
 	  //@Before
-	  public void setUp() throws Exception {
-		    driver = new FirefoxDriver();
-		    baseUrl = "http://cu156.cloud.maa.collab.net/";
-		    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-		  }
+	
 	//@Test
 	public void TestCurrentTime3() 
 	{

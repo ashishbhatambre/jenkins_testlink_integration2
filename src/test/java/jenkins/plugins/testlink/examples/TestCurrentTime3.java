6a032158b64db712a8f2830d70024173f9c62393
ashishbhatambre <iitm1710@gmail..com>
2014-06-23 12:51:52 +0530

    10th

--- src/test/java/jenkins/plugins/testlink/examples/TestCurrentTime3.java
+++ src/test/java/jenkins/plugins/testlink/examples/TestCurrentTime3.java
@@ -63,6 +63,9 @@ public class TestCurrentTime3
 		
 		Assert.assertTrue( System.currentTimeMillis() > 0 );
 		System.out.println("TestCurrentTime3 working");
+		driver = new FirefoxDriver();
+	    baseUrl = "http://cu156.cloud.maa.collab.net/";
+	    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	}
 	
 }

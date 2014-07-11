b9e3c77510196aec3e51a2c5065353f09d263c91
ashishbhatambre <iitm1710@gmail..com>
2014-07-11 18:21:20 +0530

    10th

--- src/test/java/jenkins/plugins/testlink/examples/TestCurrentTime3.java
+++ src/test/java/jenkins/plugins/testlink/examples/TestCurrentTime3.java
@@ -74,13 +74,13 @@ public class TestCurrentTime3
 		System.out.println("TestCurrentTime3 working");
 		//System.out.println(id);
 		driver = new FirefoxDriver();
-	    baseUrl = "http://cu156.cloud.maa.collab.net/";
+	    baseUrl = "https://trialzone.collab.net//";
 	    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	    driver.get(baseUrl + "/sf/sfmain/do/home");
 		driver.findElement(By.id("username")).clear();
-		driver.findElement(By.id("username")).sendKeys("admin");
+		driver.findElement(By.id("username")).sendKeys("tluser");
 		driver.findElement(By.id("pwd")).clear();
-		driver.findElement(By.id("pwd")).sendKeys("admin");
+		driver.findElement(By.id("pwd")).sendKeys("Collabnet1!");
 		driver.findElement(By.cssSelector("strong")).click();
 		// Warning: verifyTextPresent may require manual changes
 		try {
